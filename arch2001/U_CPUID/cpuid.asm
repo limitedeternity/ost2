c92b36af8fc4918c3e789d9f8f6d5561bcd1be0d
Vyacheslav Bespalov <vyatheslav2707@mail.ru>
2021-09-19 19:20:16 +0300

    CPUID test implemented

--- arch2001/U_CPUID/cpuid.asm
+++ arch2001/U_CPUID/cpuid.asm
@@ -1,6 +1,12 @@
 global main
 
-fetch_cpuid_or_die:
+section .text
+
+
+fetch_cpuid:
+    mov qword [rsp+28h], rax        ; shadow space backup (rax)
+    mov qword [rsp+20h], rcx        ; shadow space backup (rcx)
+
     sub rsp, 8                      ; 0x8 return addr padding
 
     pushfq                          ; backup rflags
@@ -13,36 +19,108 @@ fetch_cpuid_or_die:
     pop rdi                         ; move to rdi
     popfq                           ; restore backup
 
-    xor rsi, rdi                    ; xor original rflags with modified to verify write 
-    test rsi, 200000h               ; check mask (bitwise and)
-    jz exit                         ; cpuid is not available
+    xor rsi, rdi                    ; xor original rflags with modified ones to verify write 
+    test rsi, 200000h               ; [bitwise AND] check xor mask; fails => cpuid isn't available 
+    jz fetch_cpuid_failed
 
-    mov eax, 80000001h              ; Extended Processor Info and Feature Bits
+    mov rax, qword [rsp+30h]        ; restore first cpuid argument
+    mov rcx, qword [rsp+28h]        ; restore second cpuid argument
     cpuid
 
+    xor r8, r8                      ; return success status in r8 (because rax is _In_out)
     add rsp, 8
     ret
 
+fetch_cpuid_failed:
+    mov r8, -1                      ; return failure status in r8 (because rax is _In_out)
+    add rsp, 8
+    ret
+
+
 main:
-    sub rsp, 28h ; 0x8 return addr padding + rax + rcx + rbx + rdx 
+    sub rsp, 48h ; 0x8 return addr padding 
+                 ;   + rax backup (_In_out)
+                 ;   + rcx backup (_In_out)
+                 ;   + rdx backup (_Out)
+                 ;   + rbx backup (_Out)
+                 ;   + 0x20-sized shadow space (to backup arguments to be able to use reserved registers)
     
-    mov qword [rsp+20h], rax
-    mov qword [rsp+18h], rcx
-    mov qword [rsp+10h], rbx
-    mov qword [rsp+8], rdx
+    mov qword [rsp+40h], rax
+    mov qword [rsp+38h], rcx
+    mov qword [rsp+30h], rdx
+    mov qword [rsp+28h], rbx
     
-    call fetch_cpuid_or_die
+    mov rax, 80000001h       ; Extended Processor Info and Feature Bits
+    xor rcx, rcx             ; Won't be used in our case, but let's still zero it
+    call fetch_cpuid
 
-    mov rdx, qword [rsp+8]
-    mov rbx, qword [rsp+10h]
-    mov rcx, qword [rsp+18h]
-    mov rax, qword [rsp+20h]
+    test r8, r8              ; cpuid is available => 0, else => -1
+    jnz cpuid_unavail_epilogue
 
-    add rsp, 28h
-    jmp exit
+    test rdx, 10h            ; [bitwise AND] check msr feature flag
+    jnz msr_avail_epilogue 
+                             ; [logical OR]
 
-exit:
-    mov eax, 2000001h  ; exit syscall id on MacOS
-    mov edi, 0         ; status = success
+    test rdx, 800h           ; [bitwise AND] mtrr feature flag => msr present
+    jnz msr_avail_epilogue
+
+    mov rbx, qword [rsp+28h]
+    mov rdx, qword [rsp+30h]
+    mov rcx, qword [rsp+38h]
+    mov rax, qword [rsp+40h]
+
+    mov rax, 2000004h        ; write syscall id on MacOS x64
+    mov rdi, 1               ; destination = stdout
+    lea rsi, [rel msr_unavail_msg]
+    mov rdx, msr_unavail_msg.len
     syscall
+
+    add rsp, 48h
+    xor rax, rax             ; status = success
+    ret
+
+cpuid_unavail_epilogue:
+    mov rbx, qword [rsp+28h]
+    mov rdx, qword [rsp+30h]
+    mov rcx, qword [rsp+38h]
+    mov rax, qword [rsp+40h]
+
+    mov rax, 2000004h           ; write syscall id on MacOS x64
+    mov rdi, 1                  ; destination = stdout
+    lea rsi, [rel cpuid_unavail_msg]
+    mov rdx, cpuid_unavail_msg.len
+    syscall
+
+    add rsp, 48h
+    mov rax, 1                  ; status = failure
+    ret
+
+msr_avail_epilogue:
+    mov rbx, qword [rsp+28h]
+    mov rdx, qword [rsp+30h]
+    mov rcx, qword [rsp+38h]
+    mov rax, qword [rsp+40h]
+
+    mov rax, 2000004h           ; write syscall id on MacOS x64
+    mov rdi, 1                  ; destination = stdout
+    lea rsi, [rel msr_avail_msg]
+    mov rdx, msr_avail_msg.len
+    syscall
+
+    add rsp, 48h
+    xor rax, rax                ; status = success
+    ret
+
+
+section .data
+
+
+cpuid_unavail_msg: db "CPUID instruction is not available on this platform", 0ah
+.len:              equ $ - cpuid_unavail_msg
+
+msr_unavail_msg: db "Model-specific registers (MSR) are not available", 0ah
+.len:            equ $ - msr_unavail_msg
+
+msr_avail_msg: db "Model-specific registers (MSR) are available", 0ah
+.len:          equ $ - msr_avail_msg
 
